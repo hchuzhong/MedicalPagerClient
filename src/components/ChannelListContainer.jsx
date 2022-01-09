a1e02d528add726241688a62f7f3b2dd884065e2
hchuzhong <352995096@qq.com>
2022-01-09 23:41:31 +0800

    finish create channel and message group

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -6,7 +6,9 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 import HospitalIcon from "../assets/hospital.png";
 import LogoutIcon from "../assets/logout.png";
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
   <div className='channel-list__sidebar'>
     <div className='channel-list__sidebar__icon1'>
       <div className='icon1__inner'>
@@ -14,7 +16,7 @@ const SideBar = () => (
       </div>
     </div>
     <div className='channel-list__sidebar__icon2'>
-      <div className='icon2__inner'>
+      <div className='icon2__inner' onClick={logout}>
         <img src={LogoutIcon} alt='Logout' width='30' />
       </div>
     </div>
@@ -27,17 +29,43 @@ const CompanyHeader = () => (
   </div>
 );
 
-const ChannelListContainer = () => {
+const ChannelListContainer = ({
+  isCreating,
+  setIsCreating,
+  setIsEditing,
+  setCreateType,
+}) => {
+  const logout = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+    cookies.remove("phoneNumber");
+
+    window.location.reload();
+  };
+
   return (
     <>
-      <SideBar />
+      <SideBar logout={logout} />
       <div className='channel-list__list__wrapper'>
         <CompanyHeader />
         <ChannelSearch />
         <ChannelList
           filters={{}}
           channelRenderFilterFn={() => {}}
-          List={(listProps) => <TeamChannelList {...listProps} type='team' />}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type='team'
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setIsEditing={setIsEditing}
+              setCreateType={setCreateType}
+            />
+          )}
           Preview={(previewProps) => (
             <TeamChannelPreview {...previewProps} type='team' />
           )}
@@ -46,7 +74,14 @@ const ChannelListContainer = () => {
           filters={{}}
           channelRenderFilterFn={() => {}}
           List={(listProps) => (
-            <TeamChannelList {...listProps} type='messaging' />
+            <TeamChannelList
+              {...listProps}
+              type='messaging'
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setIsEditing={setIsEditing}
+              setCreateType={setCreateType}
+            />
           )}
           Preview={(previewProps) => (
             <TeamChannelPreview {...previewProps} type='messaging' />
